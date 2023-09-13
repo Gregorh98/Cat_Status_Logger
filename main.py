f9735d02d81090702379f890ec34a4aa6bc99038
Gregor <gregor.hastings@outlook.com>
2023-09-13 22:30:53 +0100

    Got longest cat time working

--- main.py
+++ main.py
@@ -71,12 +71,39 @@ def get_stats():
     with psycopg2.connect(
             "dbname='postgres' user='postgres' host='192.168.1.72' password=%s" % (os.getenv("DB_PASS"))) as conn:
         with conn.cursor() as cursor:
-            sql = "select * from public.cat_flap_history"
+            sql = "select * from public.cat_flap_history order by timestamp asc"
 
             cursor.execute(sql)
             data = cursor.fetchall()
 
-    cat_stats = {"longest_time": "TEST", "data": data}
+    max_time_gap = None
+    start_time = None
+    end_time = None
+
+    for i in range(1, len(data)):
+        if data[i - 1][2] is True and data[i][2] is False:
+            time_gap = data[i][1] - data[i - 1][1]
+            if max_time_gap is None or time_gap > max_time_gap:
+                max_time_gap = time_gap
+                start_time = data[i - 1][1]
+                end_time = data[i][1]
+
+    # Convert the max_time_gap to hours and minutes
+    hours, remainder = divmod(max_time_gap.total_seconds(), 3600)
+    minutes, _ = divmod(remainder, 60)
+
+    longest_cat_time = f"{int(hours)} hours {int(minutes)} minutes"
+
+    """
+    print("The greatest time gap between two records with True and False in the third column is:", longest_cat_time)
+    if start_time and end_time:
+        print("Start time:", start_time.strftime('%Y-%m-%d %H:%M:%S'))
+        print("End time:", end_time.strftime('%Y-%m-%d %H:%M:%S'))
+    else:
+        print("No matching records found.")
+    """
+
+    cat_stats = {"longest_time": longest_cat_time}
 
     return jsonify(cat_stats)
 
